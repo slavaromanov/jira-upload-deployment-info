e938063942d81afc2fd889a4cbcf15f79a65aeaf
Slava Romanov <slava.romanov@devunit.info>
2021-09-23 15:37:38 +0300

    yarn prune

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.submitDeploymentInfo = void 0;
 const url_1 = require("url");
 const request = require('request-promise-native');
 const dateFormat = require('dateformat');
